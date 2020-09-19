a391cb78b61e566a82eeb76b24e0c401216014a3
Taylor Darneille <tmdarneille@gmail.com>
2020-09-18 17:04:10 -0700

    all friday code done

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views (these are like your controller actions) here.
 def index(request):
@@ -8,18 +9,10 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    # Get all cats from the db
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
